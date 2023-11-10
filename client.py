92c7a811147f6a7d4464bdaab61d80c6edd6b319
violethappyjoy <21bce026@nirmauni.ac.in>
2023-11-10 23:25:58 +0530

    0.0.01

--- client.py
+++ client.py
@@ -10,14 +10,16 @@ def Main():
     speaker = AudioPlayer()
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # sock.connect((socket.gethostname(), 5555))
-    sock.connect(("10.2.90.86", 1234))
-    ctr=0
-    while True:
-        if ctr == 0:
-            msg = sock.recv(1024)
-            print(msg.decode("utf-8"))
-        audio_data = sock.recv(1024)
-        speaker.play_audio(audio_data)
+    sock.connect(("192.168.1.7", 1234))
+    audio_data = sock.recv(4096)
+    speaker.play_audio(audio_data)
+    sock.close()
+    # while True:
+        # if ctr == 0:
+        #     msg = sock.recv(1024)
+        #     print(msg.decode("utf-8"))
+        # audio_data = sock.recv(1024)
+        # speaker.play_audio(audio_data)
         
 
 if __name__=='__main__':
--- server.py
+++ server.py
@@ -6,6 +6,21 @@ import time
 from Audio.audior import AudioRecord
 from Audio.audiod import AudioPlayer
 
+def handle_client(client_socket):
+    # Create an instance of the AudioRecord class
+    mic = AudioRecord()
+
+    # Record audio
+    frames = mic.recordAudio()
+
+    # Encode audio frames into a single binary data
+    audio_data = mic.encodeFrames(frames)
+
+    # Send the audio data to the client
+    client_socket.send(audio_data)
+
+    # Close the client socket
+    client_socket.close()
 
 def Main():
     mic = AudioRecord()
@@ -15,22 +30,31 @@ def Main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # sock.bind((socket.gethostname(), 5555))
     sock.bind((IP, PORT))
-    sock.listen(5)
+    sock.listen(1)
     print(f"Server listening on port {PORT}")
-    ctr = 0
     
     while True:
-        clientSocket, address = sock.accept()
-        if ctr == 0:
-            print(f"Connection established w/ {address}")
-            clientSocket.send(bytes("Welcome to our SERVER!!", "utf-8"))
-        frames = mic.recordAudio()
-        dat = mic.encodeFrames(frames)
-        clientSocket.send(dat)
-        ctr+=1
-        if keyboard.is_pressed('esc'):
-            clientSocket.send(bytes("server shutting down", "utf-8"))
-            sys.exit()
+        client_socket, address = sock.accept()
+        print(f"Connection established with {address}")
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
+        
+    # ctr = 0
+    
+    # while True:
+    #     clientSocket, address = sock.accept()
+    #     if ctr == 0:
+    #         print(f"Connection established w/ {address}")
+    #         clientSocket.send(bytes("Welcome to our SERVER!!", "utf-8"))
+    #     # frames = mic.recordAudio()
+    #     # dat = mic.encodeFrames(frames)
+        
+    #     # clientSocket.send(dat)
+    #     ctr+=1
+    #     if keyboard.is_pressed('esc'):
+    #         clientSocket.send(bytes("server shutting down", "utf-8"))
+    #         sock.close()
+    #         sys.exit()
         
 
 if __name__=='__main__':
