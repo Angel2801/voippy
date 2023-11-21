47725e059101236613c8cd357b582429ce63c382
violethappyjoy <21bce026@nirmauni.ac.in>
2023-11-21 17:08:22 +0530

    0.2.0

--- client.py
+++ client.py
@@ -6,39 +6,24 @@ import time
 from Audio.audior import AudioRecord
 from Audio.audiod import AudioPlayer
 
+
 def Main():
     speaker = AudioPlayer()
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("192.168.1.44", 1234))
+    sock.connect(("192.168.1.46", 1234))
     
     try:
+        audio_data=b''
         while True:
-            audio_data = sock.recv(4096)  # Adjust the buffer size as needed
+            audio_data += sock.recv(65536)  # Adjust the buffer size as needed
             if not audio_data:
+                speaker.play_audio(audio_data)
                 break  # Exit the loop if no more data is received
-            speaker.play_audio(audio_data)
     except Exception as e:
         print(f"Error: {e}")
     finally:
         sock.close()
-
-# def Main():
-#     speaker = AudioPlayer()
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     # sock.connect((socket.gethostname(), 5555))
-#     sock.connect(("192.168.1.44", 1234))
-#     while True:
-#         audio_data = sock.recv(496)
-#         print(type(audio_data))
-#         speaker.play_audio(audio_data)
-#     sock.close()
-#     # while True:
-#         # if ctr == 0:
-#         #     msg = sock.recv(1024)
-#         #     print(msg.decode("utf-8"))
-#         # audio_data = sock.recv(1024)
-#         # speaker.play_audio(audio_data)
-        
+       
 
 if __name__=='__main__':
     Main()
--- server.py
+++ server.py
@@ -6,53 +6,49 @@ import time
 from Audio.audior import AudioRecord
 from Audio.audiod import AudioPlayer
 
-def handle_client(client_socket):
-    # Create an instance of the AudioRecord class
+def handle_client(client_socket, stop_event):
     mic = AudioRecord()
+    speaker = AudioPlayer()
 
-    # Record audio
-    frames = mic.recordAudio()
+    try:
+        while not stop_event.is_set():
+            # Record new audio data
+            frames = mic.recordAudio()
+            # Encode audio frames into a single binary data
+            audio_data = mic.encodeFrames(frames)
+            # Send the new audio data to the client
+            client_socket.send(audio_data)
+            audio_data_recv = b''
+            while True:
+                audio_data_recv += client_socket.recv(65536)  # Adjust the buffer size as needed
+                if not audio_data:
+                    speaker.play_audio(audio_data_recv)
+                    break  # Exit the loop if no more data is received
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        client_socket.close()
 
-    # Encode audio frames into a single binary data
-    audio_data = mic.encodeFrames(frames)
-    
-    print(audio_data)
-
-    # Send the audio data to the client
-    client_socket.send(audio_data)
-
-    # Close the client socket
-    client_socket.close()
-
-# def Main():
+# def handle_client(client_socket):
+#     # Create an instance of the AudioRecord class
 #     mic = AudioRecord()
-#     # IP = "10.2.90.86"
-#     IP = "192.168.1.44"
-#     PORT = 1234
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     # sock.bind((socket.gethostname(), 5555))
-#     sock.bind((IP, PORT))
-#     sock.listen(1)
-#     print(f"Server listening on port {PORT}")
+
+#     # Record audio
+#     frames = mic.recordAudio()
+
+#     # Encode audio frames into a single binary data
+#     audio_data = mic.encodeFrames(frames)
     
-#     while True:
-#         client_socket, address = sock.accept()
-#         print(f"Connection established with {address}")
-#         # client_socket.send(bytes("Welcome to our SERVER!!", "utf-8"))
-        
-#         # frames = mic.recordAudio()
-#         # data = mic.encodeFrames(frames)
-#         # print(type(data))
-#         # client_socket.send(data)
-        
-#         client_handler = threading.Thread(target=handle_client, args=(client_socket,))
-#         client_handler.start()
-        
-#     # ctr = 0
+#     # print(audio_data)
+
+#     # Send the audio data to the client
+#     client_socket.send(audio_data)
+
+#     # Close the client socket
+#     client_socket.close()
 
 def Main():
-    mic = AudioRecord()
-    IP = "192.168.1.44"
+    IP = "192.168.1.46"
     PORT = 1234
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((IP, PORT))
@@ -69,7 +65,7 @@ def Main():
             # Create a new thread for each client
             client_handler = threading.Thread(
                 target=handle_client,
-                args=(client_socket,),
+                args=(client_socket, stop_recording),
             )
             client_handler.start()
 
@@ -77,6 +73,35 @@ def Main():
         print("Server shutting down...")
         stop_recording.set()
         sock.close()
+
+
+# def Main():
+#     mic = AudioRecord()
+#     IP = "192.168.1.46"
+#     PORT = 1234
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     sock.bind((IP, PORT))
+#     sock.listen(1)
+#     print(f"Server listening on port {PORT}")
+
+#     stop_recording = threading.Event()
+
+#     try:
+#         while True:
+#             client_socket, address = sock.accept()
+#             print(f"Connection established with {address}")
+
+#             # Create a new thread for each client
+#             client_handler = threading.Thread(
+#                 target=handle_client,
+#                 args=(client_socket,),
+#             )
+#             client_handler.start()
+
+#     except KeyboardInterrupt:
+#         print("Server shutting down...")
+#         stop_recording.set()
+#         sock.close()
 
 if __name__=='__main__':
     Main()
